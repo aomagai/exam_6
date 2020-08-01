1d68ac22815de886aac70f87ce09c9db4b61889a
aomagai <aomagai@mail.ru>
2020-08-01 13:05:51 +0600

    добавил модель

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+STATUS_CHOICES = [
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано')
+]
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    email = models.EmailField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Почта')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    publish_at = models.DateTimeField(verbose_name="Время публикации", blank=True, default=timezone.now)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
+
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.author)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
