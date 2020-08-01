8998425ee29ece910ab555a7a13f68d79a8fc8d1
aomagai <aomagai@mail.ru>
2020-08-01 13:58:23 +0600

    добавлены базовый шаблон, главная страница, просмотр одной записи

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, book_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('book/<int:pk>/', book_view, name='book_view'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from webapp.models import Guestbook
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+def index_view(request):
+    is_admin = request.GET.get('is_admin', None)
+    if is_admin:
+        data = Guestbook.objects.all()
+    else:
+        data = Guestbook.objects.filter(status='active')
+    return render(request, 'index.html', context={
+        'books': data
+    })
+
+def book_view(request, pk):
+    book = get_object_or_404(Guestbook, pk=pk)
+    context = {'book': book}
+    return render(request, 'book_view.html', context)
